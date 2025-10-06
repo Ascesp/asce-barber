e094ace6ee4710051a2a5c830d2ea43aac898306
Ascesp <spt787289@gmail.com>
2025-10-06 13:57:33 -0600

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-// server.js
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 8080; // Muy importante
+
+// Servir todos los archivos estáticos
 app.use(express.static(path.join(__dirname)));
 
+// Servir index.html en la raíz
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
